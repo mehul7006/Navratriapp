118b66298c362c5cc57fbbe9fe3be1f96e7736a9
bunty <bunty.hada36@yahoo.com>
2026-09-11 00:52:24 +0530

    fix: aarti page top day selector also shows Day 10

--- lib/screens/organizer/aarti_management_screen.dart
+++ lib/screens/organizer/aarti_management_screen.dart
@@ -75,7 +75,7 @@ class _AartiManagementScreenState extends State<AartiManagementScreen> {
       padding: const EdgeInsets.symmetric(horizontal: 12),
       child: ListView.builder(
         scrollDirection: Axis.horizontal,
-        itemCount: 9,
+        itemCount: 10,
         itemBuilder: (context, index) {
           final day = index + 1;
           final isSelected = _selectedDay == day;
